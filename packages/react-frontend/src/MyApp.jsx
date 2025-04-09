f7ad3f1bca5ff57593312a3aea0c14f452dfb2ff
ansita13 <agrawalansita@gmail.com>
2025-04-08 20:32:52 -0700

    First React App done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,24 +5,28 @@ import Form from "./Form";
 
 function MyApp() {
 
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-    function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-          return i !== index;
-        });
-        setCharacters(updated);
-      } 
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  } 
 
-   return (
+  return (
     <div className="container">
     <Table
       characterData={characters}
       removeCharacter={removeOneCharacter}
     />
-    <Form />
+    <Form handleSubmit={updateList} />
     </div>
-);
+  );
+
+  function updateList(person){
+    setCharacters([...characters, person]);
+  }
 }
 
 export default MyApp;
