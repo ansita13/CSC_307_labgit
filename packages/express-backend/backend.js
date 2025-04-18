ea7741b7a17378168151e2b4079637aae95dcd75
ansita13 <agrawalansita@gmail.com>
2025-04-17 19:20:01 -0700

    POST/DELETE

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -14,12 +14,58 @@ const users = {
   ]
 };
 
+const findUsersByQuery = (name, job) => {
+  return users["users_list"].filter((user) => {
+    return (!name || user["name"] === name) &&
+           (!job || user["job"] === job);
+  });
+};
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+  if (index !== -1) {
+    users["users_list"].splice(index, 1);
+    return true;
+  }
+  return false;
+};
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(users);
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+
+  if (name || job) {
+    let result = findUsersByQuery(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  const addedUser = addUser(userToAdd);
+  res.status(201).send(addedUser); // 201 Created
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+  if (deleted) {
+    res.status(204).send(); // 204 No Content
+  } else {
+    res.status(404).send("User not found.");
+  }
 });
